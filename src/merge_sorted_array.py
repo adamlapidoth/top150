4aa38534dc4df7e68ec901a158df324ac7a17c4f
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-06 12:15:14 +0300

    merge_sorted_array.py: added docstring

--- src/merge_sorted_array.py
+++ src/merge_sorted_array.py
@@ -6,6 +6,14 @@ class Solution:
         self, nums1: List[int], m: int, nums2: List[int], n: int
     ) -> None:
         """
-        Do not return anything, modify nums1 in-place instead.
+        Merge nums1 and nums2 into a single array sorted in non-decreasing
+        order. Do not return anything, modify nums1 in-place instead.
+        :param nums1: A sorted array (non decreasing order).
+        :param m: Length of relevant numbers
+        :param nums2: A sorted array (non decreasing order).
+        :param n: Length of nums 2
+        :return: None. The resulting sorted array will be stored in nums1.
+        To accommodate this nums1 will have the length of m+n and the
+        last n elements will be set to 0 and should be ignored
         """
         pass
