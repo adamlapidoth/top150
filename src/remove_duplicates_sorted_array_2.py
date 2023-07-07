da639d606eb935924fbbbf69fca60bfe74c52b04
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-07 10:43:38 +0300

    remove_duplicates_sorted_array_2.py: created stub for removeDuplicates with docstring

---
+++ src/remove_duplicates_sorted_array_2.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        Remove some duplicates in-place such that each unique element appears
+        at most twice.
+        The relative order of the elements should be kept the same.
+        :param nums: A sorted array with duplicates
+        :return: The number of elements. Each element can have at most 2
+        duplicates.
+        """
+        pass
