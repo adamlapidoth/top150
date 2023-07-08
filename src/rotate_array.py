a6a907eae170bab8be47e0dd2ca84e6a5886e421
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-08 14:45:48 +0300

    rotate_array.py: created rotate array stub

---
+++ src/rotate_array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Rotate array to the right k times. do it in place.
+        :param nums: A list of numbers
+        :param k: Number of places to rotate to the right
+        """
+        pass
