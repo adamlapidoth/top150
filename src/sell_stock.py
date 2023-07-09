be90f330d52a400d4cb19e012c6cdc6e9f303f53
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-09 11:00:57 +0300

    sell_stock.py: created maxProfit stub with docstring

---
+++ src/sell_stock.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        You are given an array prices where prices[i] is the price of a given
+        stock on the ith day.
+        You want to maximize your profit by choosing a single day to buy one
+        stock and choosing a different day in the future to sell that stock.
+        Return the maximum profit you can achieve from this transaction.
+         f you cannot achieve any profit, return 0.
+        :param prices: Array of numbers
+        :return: The max profit. if no profit is possible return 0
+        """
+        pass
