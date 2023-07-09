4f2c566abafe9429e96744f00aeaeafdb628dfb1
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-09 16:41:27 +0300

    test_jump_game.py: added test cases

--- tests/test_jump_game.py
+++ tests/test_jump_game.py
@@ -9,3 +9,13 @@ def test_can_jump_1():
 def test_can_jump_2():
     s = Solution()
     assert not s.canJump([3, 2, 1, 0, 4])
+
+
+def test_can_jump_3():
+    s = Solution()
+    assert s.canJump([0])
+
+
+def test_can_jump_4():
+    s = Solution()
+    assert s.canJump([2, 5, 0, 0])
