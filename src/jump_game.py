fcb10f2731d1817e78d24d629b0ad7da24504a1c
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-09 15:15:55 +0300

    jump_game.py: implemented canJump, solution is O(n2) and wasn't excepted by leetcode

--- src/jump_game.py
+++ src/jump_game.py
@@ -1,7 +1,21 @@
-from typing import List
+from typing import List, Optional
 
 
 class Solution:
+    def can_jump_helper(self, goal: int, nums: List[int]) -> Optional[int]:
+        """
+        A helper method that returns the index from where you can jump
+        to the goal.
+        :param goal: The goal of the jump game
+        :param nums: An array of numbers
+        :return: The index of the array where jump to the goal is possible.
+        If reaching to goal is not possible from any index None is returned.
+        """
+        for i, n in enumerate(nums):
+            if nums[i] + i >= goal:
+                return i
+        return None
+
     def canJump(self, nums: List[int]) -> bool:
         """
         You are given an integer array nums. You are initially positioned at
@@ -10,4 +24,13 @@ class Solution:
         :param nums: An array of numbers
         :return: True if it is possible to reach last index, False otherwise
         """
-        pass
+        goal = len(nums) - 1
+        new_goal = self.can_jump_helper(goal, nums)
+        while new_goal:
+            new_goal = self.can_jump_helper(goal=new_goal, nums=nums[:new_goal])
+        if new_goal == 0:
+            return True
+        return False
+
+
+
