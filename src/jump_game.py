0d1b56a9c077941a4a7fb62a4427d30607ae1a64
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-09 13:36:57 +0300

    jump_game.py: created method stub with docstring

---
+++ src/jump_game.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        """
+        You are given an integer array nums. You are initially positioned at
+        the array's first index, and each element in the array represents your
+        maximum jump length at that position.
+        :param nums: An array of numbers
+        :return: True if it is possible to reach last index, False otherwise
+        """
+        pass
