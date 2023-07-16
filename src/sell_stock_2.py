56ecf0fdf193d8644ec4dca436b5b3a43baf1f96
Adam Lapidoth <adamlapidoth@gmail.com>
2023-07-16 15:56:09 +0300

    sell_stock_2.py: solution in O(n) time, O(1) space

--- src/sell_stock_2.py
+++ src/sell_stock_2.py
@@ -13,4 +13,10 @@ class Solution:
         :param prices: Array of prices
         :return: The max profit. if no profit can be achieved returns 0
         """
-        pass
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            profit = prices[i + 1] - prices[i]
+            if profit > 0:
+                max_profit += profit
+        return max_profit
+
